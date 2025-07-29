8165c4f59de87d61d34e21a8372f8081b777c47e
smitra43 <mitra.samantha5@gmail.com>
2025-07-29 14:40:35 -0400

    float comments

--- data_load.py
+++ data_load.py
@@ -1,8 +1,14 @@
-''' Preprocess and Load data'''
-
-import math
+''' Preprocess and load data '''
 import torch
-
+import math
+ 
+# Notes for Sam:
+# If you want to use gpu, you will have to use this line:
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+ 
+# then you will move everything onto the device, e.g.,
+# train_x=torch.tensor(train_x).float.to(device)
+ 
 # Training data is 100 points in [0,1] inclusive regularly spaced
 train_x = torch.linspace(0, 1, 100)
 # True function is sin(2*pi*x) with Gaussian noise
